b0ebbeff57df5462b532d1bc091f9a87a370195d
moka <qnwnen0228@gmail.com>
2023-04-17 11:07:46 +0900

    55:01 중간 커밋
    Kakao.Forms 프로젝트 생성

---
+++ KaKao.Forms/UI/Views/KakaoWindow.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace KaKao.Forms.UI.Views
+{
+    public class KakaoWindow : Control
+    {
+        static KakaoWindow()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(KakaoWindow), new FrameworkPropertyMetadata(typeof(KakaoWindow)));
+        }
+    }
+}
