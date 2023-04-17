e8c03126bf34cda55f12b06005b56681753bc228
moka <qnwnen0228@gmail.com>
2023-04-17 11:02:29 +0900

    49:58 초기 프로젝트 구성

---
+++ Kakao/Starter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Kakao
+{
+    internal class Starter
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            _ = new App().Run();
+        }
+    }
+}
